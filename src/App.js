364aced787a430cb9fbe5e17f6ed313bd9153b3d
Ben <ben.lemaitre@protonmail.com>
2020-11-26 15:23:53 +0700

    using ThemeProvider from styled-components

--- src/App.js
+++ src/App.js
@@ -10,27 +10,34 @@ import Navbar from "./components/Navbar";
 import { Container } from "./styled/Container";
 import { Main } from "./styled/Main";
 import Global from "./styled/Global";
+import { lightTheme, darkTheme } from "./styled/Themes";
+
+import { ThemeProvider } from "styled-components";
 
 function App() {
   const { loading } = useAuth0();
+  const theme = "dark";
+  const currentTheme = theme === "light" ? lightTheme : darkTheme;
 
   return (
     <Router>
-      <Global />
-      <Main>
-        {loading && <p>Loading ...</p>}
-        {!loading && (
-          <Container>
-            <Navbar />
-            <Switch>
-              <Route path="/game" component={Game} />
-              <Route path="/highScores" component={HighScores} />
-              <Route path="/gameOver" component={GameOver} />
-              <Route path="/" component={Home} />
-            </Switch>
-          </Container>
-        )}
-      </Main>
+      <ThemeProvider theme={currentTheme}>
+        <Global />
+        <Main>
+          {loading && <p>Loading ...</p>}
+          {!loading && (
+            <Container>
+              <Navbar />
+              <Switch>
+                <Route path="/game" component={Game} />
+                <Route path="/highScores" component={HighScores} />
+                <Route path="/gameOver" component={GameOver} />
+                <Route path="/" component={Home} />
+              </Switch>
+            </Container>
+          )}
+        </Main>
+      </ThemeProvider>
     </Router>
   );
 }
---
+++ src/styled/Themes.js
@@ -0,0 +1,15 @@
+const sharedStyles = {
+  accent: "#e16365",
+};
+
+export const lightTheme = {
+  mainBgColor: "#f6f6f6",
+  mainTextColor: "#333",
+  ...sharedStyles,
+};
+
+export const darkTheme = {
+  mainBgColor: "#333",
+  mainTextColor: "#f6f6f6 ",
+  ...sharedStyles,
+};
